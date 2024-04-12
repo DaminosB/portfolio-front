df9d0837b67fb89b97f488f2c10e55f92f47fcf3
Damien Bourcheix <damien@macbook-pro-de-damien.home>
2024-04-12 17:49:09 +0200

    Export of the calcCoordinates func

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -6,36 +6,7 @@ import styles from "./ProjectCard.module.css";
 import { useEffect, useRef } from "react";
 
 // When this func is called, it calculates the coordinates of the ProjectCard in the ProjectsContainer
-const calcCoordinates = (index, domElement) => {
-  // index: Number. The position of the element in the projectsToDisplay array
-  // domElement. The ProjectCards whose coordinates are going to be calculates
-
-  // We begin by getting the CardProjects dimensions (all CardProject's have the same)
-  const cardSize = {
-    width: domElement.offsetWidth,
-    height: domElement.offsetHeight,
-  };
-
-  // We need to find the parent's width
-  const parentWidth = domElement.parentNode.offsetWidth;
-
-  // With card's width, we get the number of elemennts per row
-  const elementsPerRow = Math.floor(parentWidth / cardSize.width);
-
-  // Knowing this, we can determine on which Row the card is displayed
-  const rowIndex = Math.floor(index / elementsPerRow);
-
-  // To get the columnIndex, we need to remove all the elements displayed on the previous rows from the current index
-  const columnIndex = index - rowIndex * elementsPerRow;
-
-  // This lets us calculte the coordinates of the card
-  const topPosition = rowIndex * cardSize.height + rowIndex * 20;
-  const leftPosition = columnIndex * cardSize.width + columnIndex * 20;
-
-  // We apply them the card
-  domElement.style.top = `${topPosition}px`;
-  domElement.style.left = `${leftPosition}px`;
-};
+import calcCoordinates from "@/utils/calcCoordinates";
 
 const showElement = (domElement) => {
   domElement.style.visibility = "visible";
@@ -66,7 +37,7 @@ const ProjectCard = ({ project, filterArray, projectsToDisplay }) => {
   }, [projectsToDisplay]);
 
   return (
-    <div key={project.id} className={styles.projectCard} ref={cardRef}>
+    <div key={project.id} ref={cardRef} className={styles.projectCard}>
       <img
         src={project.thumbnail.url}
         alt={project.thumbnail.alternativeText}
---
+++ src/utils/calcCoordinates.js
@@ -0,0 +1,32 @@
+const calcCoordinates = (index, domElement) => {
+  // index: Number. The position of the element in the projectsToDisplay array
+  // domElement. The ProjectCards whose coordinates are going to be calculates
+
+  // We begin by getting the CardProjects dimensions (all CardProject's have the same)
+  const cardSize = {
+    width: domElement.offsetWidth,
+    height: domElement.offsetHeight,
+  };
+
+  // We need to find the parent's width
+  const parentWidth = domElement.parentNode.offsetWidth;
+
+  // With the card's width, we get the number of elemennts per row
+  const elementsPerRow = Math.floor(parentWidth / cardSize.width);
+
+  // Knowing this, we can determine on which Row the card is displayed
+  const rowIndex = Math.floor(index / elementsPerRow);
+
+  // To get the columnIndex, we need to remove all the elements displayed on the previous rows from the current index
+  const columnIndex = index - rowIndex * elementsPerRow;
+
+  // This lets us calculate the coordinates of the card, 20 being the gap between each card
+  const topPosition = rowIndex * cardSize.height + rowIndex * 20;
+  const leftPosition = columnIndex * cardSize.width + columnIndex * 20;
+
+  // We apply them the card
+  domElement.style.top = `${topPosition}px`;
+  domElement.style.left = `${leftPosition}px`;
+};
+
+export default calcCoordinates;
