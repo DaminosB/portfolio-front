e79d47fae0dc99ae5bf5e112901119f12251be9b
Damien Bourcheix <damien@macbook-pro-de-damien.home>
2024-05-07 11:46:09 +0200

    Correction of the deformation on images at container height change

--- src/utils/calcSliderTranslation.js
+++ src/utils/calcSliderTranslation.js
@@ -1,3 +1,67 @@
+// const calcSliderTranslation = (targetIndex) => {
+//   const [headerNode] = document.getElementsByTagName("HEADER");
+
+//   const sliderElement = document.getElementById("slider");
+
+//   const activeSectionTopPosition =
+//     sliderElement.children[targetIndex].offsetTop;
+
+//   const sliderHasCover = sliderElement.children[0].id.includes("cover");
+//   // Index 0 is the cover component and has a special treatment
+//   if (sliderHasCover) {
+//     const coverComponent = sliderElement.children[0];
+
+//     switch (targetIndex) {
+//       case 0:
+//         // If the targetIndex is 0, we are on the cover, and we should remove classes hidden and removed
+//         coverComponent.classList.remove("hidden");
+//         coverComponent.classList.remove("scaled-down");
+//         headerNode.classList.remove("zero-height");
+//         headerNode.classList.remove("hidden");
+
+//         // We also slide to the cover component
+//         sliderElement.style.transform = `translateY(${-activeSectionTopPosition}px)`;
+
+//         break;
+
+//       case 1:
+//         // We check if the cover if the cover has a hidden class
+//         const coverIsShown = !Array.from(coverComponent.classList).includes(
+//           "hidden"
+//         );
+//         // If not, it means the cover is shown
+
+//         if (coverIsShown) {
+//           // If so, we reduce its size and opacity
+//           coverComponent.classList.add("hidden");
+//           coverComponent.classList.add("scaled-down");
+//           headerNode.classList.add("zero-height");
+//           headerNode.classList.add("hidden");
+//         } else {
+//           // If not, we just slide the active child
+//           sliderElement.style.transform = `translateY(${-activeSectionTopPosition}px)`;
+//         }
+//         break;
+
+//       default:
+//         // All other scenarios, we slide normaly
+//         sliderElement.style.transform = `translateY(${-activeSectionTopPosition}px)`;
+//         break;
+//     }
+//   } else {
+//     // We check if the cover if the cover has a hidden class
+//     const headerIsHidden = Array.from(headerNode.classList).includes("hidden");
+//     // If not, it means the header is shown
+//     if (headerIsHidden) {
+//       headerNode.classList.remove("zero-height");
+//       headerNode.classList.remove("hidden");
+//     }
+//     sliderElement.style.transform = `translateY(${-activeSectionTopPosition}px)`;
+//   }
+// };
+
+// export default calcSliderTranslation;
+
 const calcSliderTranslation = (targetIndex) => {
   const [headerNode] = document.getElementsByTagName("HEADER");
 
@@ -7,6 +71,11 @@ const calcSliderTranslation = (targetIndex) => {
     sliderElement.children[targetIndex].offsetTop;
 
   const sliderHasCover = sliderElement.children[0].id.includes("cover");
+
+  const clientWindowWidth = window.innerWidth;
+
+  if (clientWindowWidth < 1024) {
+  }
   // Index 0 is the cover component and has a special treatment
   if (sliderHasCover) {
     const coverComponent = sliderElement.children[0];
@@ -49,7 +118,7 @@ const calcSliderTranslation = (targetIndex) => {
         break;
     }
   } else {
-    // We check if the cover if the cover has a hidden class
+    // We check if the header has a hidden class
     const headerIsHidden = Array.from(headerNode.classList).includes("hidden");
     // If not, it means the header is shown
     if (headerIsHidden) {
