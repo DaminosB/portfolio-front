08c4fed4eb039c700548c72408ed078eecab3ea1
Damien Bourcheix <damien@macbook-pro-de-damien.home>
2024-05-29 18:43:32 +0200

    RelatedProjects component added

--- src/components/RelatedProjects/RelatedProjects.js
+++ src/components/RelatedProjects/RelatedProjects.js
@@ -1,7 +1,32 @@
+import axios from "axios";
 import styles from "./RelatedProjects.module.css";
+import ProjectCardsWrapper from "../ProjectCardsWrapper/ProjectCardsWrapper";
+import ProjectCard from "../ProjectCard/ProjectCard";
 
-const RelatedProjects = () => {
-  return <section>RelatedProjects</section>;
+const RelatedProjects = ({ relatedProjects, style }) => {
+  const totalGapWidth = (style.thumbnailsPerRow - 1) * style.gap;
+  const projectCardWidthStr = `calc((100% - ${totalGapWidth}px) / ${style.thumbnailsPerRow})`;
+
+  const cardsToDisplay = relatedProjects.map((relatedProject) => ({
+    id: relatedProject.id,
+    thumbnail: relatedProject.thumbnail,
+    tags: relatedProject.tags,
+    link: `/projects/${relatedProject.id}`,
+    customStyles: {
+      width: projectCardWidthStr,
+      aspectRatio: "4/5",
+    },
+  }));
+
+  return (
+    <section>
+      <ProjectCardsWrapper style={style} cardsToDisplay={cardsToDisplay}>
+        {cardsToDisplay.map((card) => {
+          return <ProjectCard key={card.id} cardData={card} />;
+        })}
+      </ProjectCardsWrapper>
+    </section>
+  );
 };
 
 export default RelatedProjects;
