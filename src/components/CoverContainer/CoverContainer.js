0cc9089b69d3bae0e3fc0f3cbdac116cefd53bf1
Damien Bourcheix <damien@mbp-de-damien.home>
2024-06-27 16:18:09 +0200

    CoverContainer responsive

--- src/components/CoverContainer/CoverContainer.js
+++ src/components/CoverContainer/CoverContainer.js
@@ -1,9 +1,15 @@
 import styles from "./CoverContainer.module.css";
 
-const CoverContainer = ({ coverUrl, coverAltTxt }) => {
+const CoverContainer = ({ coverUrl, coverAltTxt, customColors }) => {
+  const inlineStyle = {
+    color: customColors.secondaryColor,
+  };
   return (
     <section className={styles.coverContainer} id="cover-container">
       <img src={coverUrl} alt={coverAltTxt} />
+      <div style={inlineStyle} className={styles.altCover}>
+        <img src={coverUrl} alt={coverAltTxt} />
+      </div>
     </section>
   );
 };
