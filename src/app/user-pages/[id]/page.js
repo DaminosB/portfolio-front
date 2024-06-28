4e6d854933d84a6800352080fd10e2c9ac0d2b0f
Damien Bourcheix <damien@mbp-de-damien.home>
2024-06-28 10:59:14 +0200

    CustomColors object passed to modules

--- src/app/user-pages/[id]/page.js
+++ src/app/user-pages/[id]/page.js
@@ -30,15 +30,31 @@ export default async function ProjectsIdPage({ params }) {
         {page.modules.map((module, index) => {
           switch (module.__component) {
             case "module.pleine-page":
-              return <Module_Fullpage key={module.id} module={module} />;
+              return (
+                <Module_Fullpage
+                  key={module.id}
+                  module={module}
+                  customColors={customColors}
+                />
+              );
 
             case "module.colonne-multi-images":
               return (
-                <Module_MultiImagesColumn key={module.id} module={module} />
+                <Module_MultiImagesColumn
+                  key={module.id}
+                  module={module}
+                  customColors={customColors}
+                />
               );
 
             case "module.container":
-              return <Module_Container key={module.id} module={module} />;
+              return (
+                <Module_Container
+                  key={module.id}
+                  module={module}
+                  customColors={customColors}
+                />
+              );
 
             default:
               break;
