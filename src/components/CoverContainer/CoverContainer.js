cffc80b7b3a462f6ecae09c88766d68022073747
Damien Bourcheix <damien@mbp-de-damien.home>
2024-09-18 15:27:35 +0200

    background div style simplified

--- src/components/CoverContainer/CoverContainer.js
+++ src/components/CoverContainer/CoverContainer.js
@@ -1,16 +1,44 @@
+"use client";
+
 import styles from "./CoverContainer.module.css";
 
+import { useContext, useRef, useEffect, useState } from "react";
+
+import { LayoutContext } from "@/wrappers/LayoutWrapper/LayoutWrapper";
+
 const CoverContainer = ({ coverUrl, coverAltTxt, customColors }) => {
-  const inlineStyle = {
+  const { layoutScrollPos } = useContext(LayoutContext);
+
+  const [hideCover, setHideCover] = useState(false);
+
+  const coverRef = useRef(null);
+
+  useEffect(() => {
+    const coverElem = coverRef.current;
+
+    const isDisplayed = coverElem.offsetTop === layoutScrollPos;
+
+    if (isDisplayed) setHideCover(false);
+    else setHideCover(true);
+  }, [layoutScrollPos]);
+
+  const containerInlineStyle = {
     color: customColors.secondaryColor,
   };
+
+  const backgroundInlineStyle = {
+    background: `url(${coverUrl}) no-repeat center / auto 100%`,
+  };
+
   return (
-    <section className={styles.coverContainer} id="cover-container">
+    <div
+      className={`${styles.coverContainer} ${hideCover && "hidden"}`}
+      ref={coverRef}
+      style={containerInlineStyle}
+    >
+      <div style={backgroundInlineStyle}></div>
       <img src={coverUrl} alt={coverAltTxt} />
-      <div style={inlineStyle} className={styles.altCover}>
-        <img src={coverUrl} alt={coverAltTxt} />
-      </div>
-    </section>
+    </div>
   );
 };
 
