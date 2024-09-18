4d395fba9daca318b4493d294af0d08a61bd6f7e
Damien Bourcheix <damien@mbp-de-damien.home>
2024-09-18 15:47:50 +0200

    Now known as SidePanelNavigation

--- src/components/MediasNavigation/MediasNavigation.js
+++
@@ -1,7 +0,0 @@
-import styles from "./MediasNavigation.module.css";
-
-const MediasNavigation = () => {
-  return <nav className={styles.mediasNavigation}>MediasNavigation</nav>;
-};
-
-export default MediasNavigation;
