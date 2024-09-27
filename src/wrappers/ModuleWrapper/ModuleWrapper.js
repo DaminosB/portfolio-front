247a9cb2266bc600549d2adf3292581a66c3dd88
Damien Bourcheix <damien@mbp-de-damien.home>
2024-09-27 13:31:40 +0200

    Sync scroll function integrated to the component because of the scrollSticky hook removal

--- src/wrappers/ModuleWrapper/ModuleWrapper.js
+++ src/wrappers/ModuleWrapper/ModuleWrapper.js
@@ -2,7 +2,7 @@
 import styles from "./ModuleWrapper.module.css";
 
 // React hooks imports
-import { useState, useContext, useEffect, createContext } from "react";
+import { useState, useContext, useEffect, useRef, createContext } from "react";
 import { LayoutContext } from "../LayoutWrapper/LayoutWrapper";
 import { SnapScrollerContext } from "../SnapScrollWrapper/SnapScrollWrapper";
 
@@ -10,24 +10,27 @@ export const ModuleContext = createContext();
 
 // Components imports
 import Carousel from "@/components/Carousel/Carousel";
+import useScrollTracker from "@/hooks/useScrollTracker";
 
-import useScrollSticky from "@/hooks/useScrollSticky";
-
-const ModuleWrapper = ({
-  medias,
-  inlineStyle,
-  customColors,
-  sectionId,
-  children,
-}) => {
+const ModuleWrapper = ({ medias, inlineStyle, customColors, children }) => {
   // This state is changed when the viewer clicks the content
   // On false, none of the dragging effects are active
   const [isActiveSection, setIsActiveSection] = useState(false);
-  const { activeCoordinates, openModale } = useContext(LayoutContext);
+  const { activeCoordinates, openModale, isModaleDisplayed, getSectionCoords } =
+    useContext(LayoutContext);
+  const [activeContainerIndex, activeChildIndex] = activeCoordinates;
+  // const { getSectionCoords } = useContext(SnapScrollerContext);
+
+  const sectionRef = useRef(null);
+
+  const sectionCoordsRef = useRef(null);
+
+  const movableElemPositions = useRef([]);
+
+  const { scrollTrack, scrollPosition } = useScrollTracker();
 
   const openCarousel = (mediaId) => {
     const mediaIndex = medias.findIndex((media) => media.id === mediaId);
-
     openModale(
       customColors,
       <Carousel
@@ -38,52 +41,73 @@ const ModuleWrapper = ({
     );
   };
 
-  const { scrollBoundedChildSync } = useScrollSticky();
+  const syncScroll = (scrollPosition) => {
+    const sectionNode = sectionRef.current;
 
-  const [activeContainerIndex, activeChildIndex] = activeCoordinates;
+    const movableElements = Array.from(sectionNode.firstElementChild.children);
 
-  const { getSectionCoords } = useContext(SnapScrollerContext);
+    const initialPositionFirstElement = movableElements[0].offsetTop;
 
-  const contextValues = { isActiveSection, openCarousel };
+    const allElementsAligned = movableElements.every(
+      (elem) => elem.offsetTop === initialPositionFirstElement
+    );
+
+    if (!allElementsAligned) return;
+
+    movableElements.forEach((element, i) => {
+      const translateValue = movableElemPositions.current[i] || 0;
+
+      const elemBottomPosition =
+        translateValue + (element.scrollHeight - scrollPosition);
+
+      if (
+        elemBottomPosition < element.offsetHeight &&
+        sectionNode.scrollHeight > element.scrollHeight
+      ) {
+        const newTranslateValue =
+          scrollPosition + (element.offsetHeight - element.scrollHeight);
+        element.style.transform = `translateY(${newTranslateValue}px)`;
+        movableElemPositions.current[i] = newTranslateValue;
+      } else if (translateValue > scrollPosition) {
+        element.style.transform = `translateY(${scrollPosition}px)`;
+        movableElemPositions.current[i] = scrollPosition;
+      }
+    });
+  };
 
   useEffect(() => {
-    const sectionCoords = getSectionCoords(sectionId);
-    const [containerIndex, sectionIndex] = sectionCoords;
+    if (!sectionCoordsRef.current)
+      sectionCoordsRef.current = getSectionCoords(sectionRef.current);
 
-    const activeStatus =
+    const [containerIndex, sectionIndex] = sectionCoordsRef.current;
+
+    const isActive =
       activeContainerIndex === containerIndex &&
       activeChildIndex === sectionIndex;
 
-    setIsActiveSection(activeStatus);
+    if (isActive) {
+      setIsActiveSection(true);
+      syncScroll(scrollPosition);
+    } else setIsActiveSection(false);
   }, [
     activeCoordinates,
     activeChildIndex,
     activeContainerIndex,
     getSectionCoords,
-    sectionId,
+    scrollPosition,
   ]);
+
+  const contextValues = { isActiveSection, openCarousel, isModaleDisplayed };
 
   return (
     <ModuleContext.Provider value={contextValues}>
       <section
+        className={styles.moduleWrapper}
         style={inlineStyle}
-        id={sectionId}
-        onScroll={scrollBoundedChildSync}
+        ref={sectionRef}
+        onScroll={scrollTrack}
       >
         {children}
-        {/* {showModale &&
-          createPortal(
-            <ModaleWrapper
-              customColors={customColors}
-              exitFunction={handleExitModale}
-            >
-              <img
-                src={modaleContent.url}
-                alt={modaleContent.alternativeText}
-              />
-            </ModaleWrapper>,
-            document.body
-          )} */}
       </section>
     </ModuleContext.Provider>
   );
