65982b0e3bedd6b1cf9dceb95d7143f117fc2213
Damien Bourcheix <damien@mbp-de-damien.home>
2024-10-08 16:10:17 +0200

    Adapatation to new Modale and EndScrollPanel components

--- src/wrappers/LayoutWrapper/LayoutWrapper.js
+++ src/wrappers/LayoutWrapper/LayoutWrapper.js
@@ -2,109 +2,94 @@
 
 import styles from "./LayoutWrapper.module.css";
 
+// Hooks import
 import { createContext, useEffect, useState, useCallback, useRef } from "react";
-
-import { usePathname, useRouter } from "next/navigation";
-
+import { usePathname } from "next/navigation";
 import useScrollTracker from "@/hooks/useScrollTracker";
-
-import ModalePortal from "../ModalePortal/ModalePortal";
 
 export const LayoutContext = createContext();
 
+// This component serves as a wrapper for every page, setting up key parameters and states
 const LayoutWrapper = ({ children }) => {
-  const [activeCoordinates, setActiveCoordinates] = useState([0, 0]);
+  // Stores the coordinates of the currently visible content.
+  // [Index of the container, Index of the currently active section/element]
+  const [activeCoords, setActiveCoords] = useState([0, 0]);
+
+  // Stores the active section/element index for each container (which child within the container is being viewed)
   const [containersPositions, setContainersPositions] = useState([]);
 
-  const [isModaleDisplayed, setIsModaleDisplayed] = useState(false);
+  // Controls the visibility of the bottom panel and manages popups/modals
+  const [endScrollValue, setEndScrollValue] = useState(false);
+  const [modaleContent, setModaleContent] = useState(null);
 
-  const router = useRouter();
-
-  const [modaleContent, setModaleContent] = useState({
-    customColors: { mainColor: "", secondaryColor: "" },
-    domElement: null,
-  });
+  // Custom hook to track the scroll position and determine which section is in view
   const {
-    scrollTrack,
-    activeChildIndex: activeContainerIndex,
-    scrollPosition,
+    displayIndex: activeContainerIndex, // Index of the currently visible container
+    scrollPosition, // Current scrollTop value of the container
+    scrollTrack, // Function to handle the onScroll event
   } = useScrollTracker();
 
+  // Tracks the current URL to detect page changes
+  const pathname = usePathname();
+  const cachedPathname = useRef(null);
+
+  // Ref to store the layout container DOM element
   const layoutRef = useRef(null);
 
-  const pathname = usePathname();
-  const cachedPathname = useRef(pathname);
-
+  // Function to update the position of containers and the currently visible child within each container
   const updateContainerPos = useCallback((container, newIndex) => {
     const layoutNode = layoutRef.current;
     const containersArray = Array.from(layoutNode.children);
 
+    // Finds the index of the container in the layout
     const containerIndex = containersArray.findIndex(
       (child) => child === container
     );
 
+    // Updates the state with the new index for the container's active section
     setContainersPositions((prev) => {
-      const newTab = [...prev];
-      newTab[containerIndex] = newIndex;
-      return newTab;
+      const updatedPositions = [...prev];
+      updatedPositions[containerIndex] = newIndex;
+      return updatedPositions;
     });
   }, []);
 
+  // Effect triggered by scroll events and page changes to update the active coordinates
   useEffect(() => {
     const layoutNode = layoutRef.current;
+
     const activeChildIndex = containersPositions[activeContainerIndex] || 0;
 
-    setActiveCoordinates([activeContainerIndex, activeChildIndex]);
+    // Checks if the active coordinates have changed (container or section)
+    const coordsHaveChanged =
+      activeCoords[0] !== activeContainerIndex ||
+      activeCoords[1] !== activeChildIndex;
 
+    if (coordsHaveChanged)
+      setActiveCoords([activeContainerIndex, activeChildIndex]);
+
+    // On page change, resets state and clears bottom panel and modal
     if (pathname !== cachedPathname.current) {
       cachedPathname.current = pathname;
-
-      layoutNode.parentNode.style.opacity = 1;
       const childrenArray = Array.from(layoutNode.children);
       setContainersPositions(() => childrenArray.map(() => 0));
+      setEndScrollValue(0);
+      setModaleContent(null);
     }
-  }, [activeContainerIndex, containersPositions, pathname]);
+  }, [activeCoords, activeContainerIndex, containersPositions, pathname]);
 
-  const linkTo = (link) => {
-    const layoutNode = layoutRef.current;
-
-    layoutNode.parentNode.style.opacity = 0;
-    setTimeout(() => {
-      layoutNode.scrollTo({ top: 0, behavior: "instant" });
-      router.push(link, { scroll: false });
-    }, 150);
-  };
-
-  const [showModale, setShowModale] = useState(false);
-
-  const openModale = (customColors, domElement) => {
-    setModaleContent({ customColors, domElement });
-
-    setShowModale(true);
-    setIsModaleDisplayed(true);
-  };
-
-  const closeModale = () => {
-    setShowModale(false);
-    setIsModaleDisplayed(false);
-  };
-
-  const resetModaleContent = () => {
-    setModaleContent({
-      customColors: { mainColor: "", secondaryColor: "" },
-      domElement: null,
-    });
-  };
-
+  // Provides context values to children components for managing layout, scroll, and popup states
   const contextValues = {
-    activeCoordinates,
+    activeCoords,
     layoutScrollPos: scrollPosition,
     layoutNode: layoutRef.current,
-    isModaleDisplayed,
-    openModale,
+    showModale: modaleContent ? true : false, // Boolean to determine if modal is active
     updateContainerPos,
     getSectionCoords,
-    linkTo,
+    endScrollValue,
+    setEndScrollValue,
+    modaleContent,
+    setModaleContent,
   };
 
   return (
@@ -116,25 +101,21 @@ const LayoutWrapper = ({ children }) => {
       >
         {children}
       </div>
-      <ModalePortal
-        showModale={showModale}
-        closeFunc={closeModale}
-        resetContent={resetModaleContent}
-        customColors={modaleContent.customColors}
-      >
-        {modaleContent.domElement}
-      </ModalePortal>
     </LayoutContext.Provider>
   );
 };
 
+// Helper function to get the container and section indices for a given node
 const getSectionCoords = (sectionNode) => {
   const findIndexInParent = (parent, targetChild) =>
     Array.from(parent.children).findIndex((child) => child === targetChild);
 
   const container = sectionNode.parentNode;
 
+  // Finds the index of the container in its parent node
   const containerIndex = findIndexInParent(container.parentNode, container);
+
+  // Finds the index of the section (child) in the container
   const sectionIndex = findIndexInParent(container, sectionNode);
 
   return [containerIndex, sectionIndex];
