05d1e1de669b5f8adcd8994bf94077d64e8a1fe8
Damien Bourcheix <damien@mbp-de-damien.home>
2024-10-17 16:36:54 +0200

    Text styling moved to new TextWrapper

--- src/utils/generateInlineStyle.js
+++ src/utils/generateInlineStyle.js
@@ -1,7 +1,7 @@
 const generateInlineStyle = (module) => {
   // // This func returns an object with the CSS properties of the modules that calls it
   // First we extract each key of the objetc
-  const { backgroundImage, backgroundColor, text, medias, gap } = module;
+  const { backgroundImage, backgroundColor, medias, gap } = module;
 
   // Then we prepare the object we will return
   const response = {
@@ -16,23 +16,9 @@ const generateInlineStyle = (module) => {
   if (backgroundImage)
     response.sectionStyle.backgroundImage = `url(${backgroundImage.url})`;
 
-  if (medias.length > 1) {
-    // If multiple medias must be displayed, we apply the gap value the user has stored
-    response.mediasContainerStyle.gap = `${gap}px`;
-  }
+  // If multiple medias must be displayed, we apply the gap value the user has stored
+  if (medias.length > 1) response.mediasContainerStyle.gap = `${gap}px`;
 
-  // If the module contains text, more parameters must be set
-  if (text) {
-    response.contentDivStyle.fontFamily = text.font
-      .substring(0, text.font.indexOf("("))
-      .trim();
-
-    // Text decorations
-    response.contentDivStyle.color = text.textColor;
-
-    // Gap between text and medias
-    response.contentDivStyle.gap = `${text.gap}px`;
-  }
   return response;
 };
 
