d6251b6eba83b4fc229cd35230efee224611cb42
Damien Bourcheix <damien@mbp-de-damien.home>
2024-10-17 16:38:41 +0200

    Updated to comply with new back parameters regarding text

--- src/wrappers/TextWrapper/TextWrapper.js
+++ src/wrappers/TextWrapper/TextWrapper.js
@@ -1,13 +1,34 @@
-"use client";
-
 import styles from "./TextWrapper.module.css";
 
-const TextWrapper = ({ children }) => {
+const TextWrapper = ({ textModule, children }) => {
+  const inlineStyle = generateTextStyle(textModule);
+
   return (
-    <div className={styles.textContainer}>
-      <p>{children}</p>
+    <div className={styles.textWrapper} style={inlineStyle}>
+      <div>{children}</div>
     </div>
   );
 };
 
 export default TextWrapper;
+
+const generateTextStyle = (textModule) => {
+  const { alignment, font, textColor } = textModule;
+
+  const response = {
+    fontFamily: "",
+    color: "",
+    textAlign: "",
+  };
+
+  response.fontFamily = font.substring(0, font.indexOf("(")).trim();
+
+  response.color = textColor;
+
+  if (alignment === "Centré") response.textAlign = "center";
+  else if (alignment === "Gauche") response.textAlign = "left";
+  else if (alignment === "Droite") response.textAlign = "right";
+  else if (alignment === "Justifié") response.textAlign = "justified";
+
+  return response;
+};
