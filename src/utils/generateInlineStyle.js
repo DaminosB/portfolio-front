b2f74e9869b9affce787a020858246fdb8875b53
Damien Bourcheix <damien@mbp-de-damien.home>
2024-10-22 14:52:23 +0200

    Adaptation to new mediaBlocks key

--- src/utils/generateInlineStyle.js
+++ src/utils/generateInlineStyle.js
@@ -1,7 +1,7 @@
 const generateInlineStyle = (module) => {
   // // This func returns an object with the CSS properties of the modules that calls it
   // First we extract each key of the objetc
-  const { backgroundImage, backgroundColor, medias, gap } = module;
+  const { backgroundImage, backgroundColor, mediaBlocks, gap } = module;
 
   // Then we prepare the object we will return
   const response = {
@@ -17,7 +17,8 @@ const generateInlineStyle = (module) => {
     response.sectionStyle.backgroundImage = `url(${backgroundImage.url})`;
 
   // If multiple medias must be displayed, we apply the gap value the user has stored
-  if (medias.length > 1) response.mediasContainerStyle.gap = `${gap}px`;
+  if (mediaBlocks && mediaBlocks.length > 1)
+    response.mediasContainerStyle.gap = `${gap}px`;
 
   return response;
 };
