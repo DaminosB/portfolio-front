c168c298b474d80a5e872031377ad0cef27954f3
Damien Bourcheix <damien@mbp-de-damien.home>
2024-11-07 16:07:59 +0100

    medias galleries handling added with event listeners for navigation

--- src/constructors/ModuleWrapper/ModuleWrapper.js
+++ src/constructors/ModuleWrapper/ModuleWrapper.js
@@ -4,86 +4,64 @@ import styles from "./ModuleWrapper.module.css";
 // React hooks imports
 import { useState, useContext, useEffect, useRef, createContext } from "react";
 import { LayoutContext } from "@/wrappers/LayoutWrapper/LayoutWrapper";
-import { SnapScrollerContext } from "@/wrappers/SnapScrollWrapper/SnapScrollWrapper";
 
 export const ModuleContext = createContext();
 
 // Components imports
 import Carousel from "@/components/Carousel/Carousel";
-import useScrollTracker from "@/hooks/useScrollTracker";
 
-const ModuleWrapper = ({ medias, inlineStyle, customColors, children }) => {
+const ModuleWrapper = ({ inlineStyle, customColors, module, children }) => {
   // This state is changed when the viewer clicks the content
   // On false, none of the dragging effects are active
   const [isActiveSection, setIsActiveSection] = useState(false);
+
   const { activeCoords, showModale, getSectionCoords, setModaleContent } =
     useContext(LayoutContext);
   const [activeContainerIndex, activeChildIndex] = activeCoords;
 
   const sectionRef = useRef(null);
-
   const sectionCoordsRef = useRef(null);
 
-  const movableElemPositions = useRef([]);
+  const [galleryIndex, setGalleryIndex] = useState(0);
 
-  const { scrollTrack, scrollPosition } = useScrollTracker();
+  const maxGalleryIndex = module.mediaBlocks.reduce(
+    (maxAssetsCount, mediaBlock) =>
+      Math.max(maxAssetsCount, mediaBlock.mediaAssets.length - 1),
+    0
+  );
+
+  const mediasToDisplay = module.mediaBlocks
+    ? module.mediaBlocks
+        .flatMap((mediaBlock) =>
+          mediaBlock.mediaAssets.filter(
+            (mediaAsset) => mediaAsset.addToCarousel
+          )
+        )
+        .filter(
+          (mediaAsset, index, array) =>
+            index === array.findIndex((subItem) => subItem.id === mediaAsset.id)
+        )
+    : null;
 
   const openCarousel = (mediaId) => {
-    const mediaIndex = medias.findIndex((media) => media.id === mediaId);
+    const mediaIndex = mediasToDisplay.findIndex(
+      (media) => media.id === mediaId
+    );
     setModaleContent(
       <Carousel
-        medias={medias}
+        mediasToDisplay={mediasToDisplay}
+        mediaBlocks={module.mediaBlocks}
         indexStart={mediaIndex}
         customColors={customColors}
       />
     );
   };
 
-  const syncScroll = (scrollPosition) => {
-    const sectionNode = sectionRef.current;
-
-    const movableElements = Array.from(sectionNode.firstElementChild.children);
-
-    const initialPositionFirstElement = movableElements[0].offsetTop;
-
-    const allElementsAligned = movableElements.every(
-      (elem) => elem.offsetTop === initialPositionFirstElement
-    );
-
-    if (!allElementsAligned || movableElements.length === 1) return;
-
-    const tallestElemIndex = movableElements
-      .map((element) => element.scrollHeight)
-      .reduce(
-        (maxIndex, currentValue, currentIndex, scrollHeights) =>
-          currentValue > scrollHeights[maxIndex] ? currentIndex : maxIndex,
-        0
-      );
-
-    movableElements.forEach((element, i) => {
-      const translateValue = movableElemPositions.current[i] || 0;
-
-      const elemBottomPosition =
-        translateValue + (element.scrollHeight - scrollPosition);
-
-      if (
-        elemBottomPosition < sectionNode.offsetHeight &&
-        i !== tallestElemIndex
-      ) {
-        const newTranslateValue =
-          scrollPosition + (element.offsetHeight - element.scrollHeight);
-        element.style.transform = `translateY(${newTranslateValue}px)`;
-        movableElemPositions.current[i] = newTranslateValue;
-      } else if (translateValue > scrollPosition) {
-        element.style.transform = `translateY(${scrollPosition}px)`;
-        movableElemPositions.current[i] = scrollPosition;
-      }
-    });
-  };
-
   useEffect(() => {
+    const section = sectionRef.current;
+
     if (!sectionCoordsRef.current)
-      sectionCoordsRef.current = getSectionCoords(sectionRef.current);
+      sectionCoordsRef.current = getSectionCoords(section);
 
     const [containerIndex, sectionIndex] = sectionCoordsRef.current;
 
@@ -91,28 +69,77 @@ const ModuleWrapper = ({ medias, inlineStyle, customColors, children }) => {
       activeContainerIndex === containerIndex &&
       activeChildIndex === sectionIndex;
 
-    if (isActive) {
-      setIsActiveSection(true);
-      syncScroll(scrollPosition);
-    } else setIsActiveSection(false);
-  }, [
-    activeCoords,
-    activeChildIndex,
-    activeContainerIndex,
-    getSectionCoords,
-    scrollPosition,
-  ]);
+    setIsActiveSection(isActive);
+  }, [activeCoords, activeChildIndex, activeContainerIndex, getSectionCoords]);
 
-  const contextValues = { isActiveSection, openCarousel, showModale };
+  const contextValues = {
+    isActiveSection,
+    openCarousel,
+    showModale,
+    galleryIndex,
+    setGalleryIndex,
+  };
 
-  const handleStopPropagation = (e) => {
-    const sectionNode = sectionRef.current;
+  const wheelDebounce = useRef(true);
 
-    const isAtBottom =
-      sectionNode.scrollHeight - (scrollPosition + sectionNode.offsetHeight) <
-      1;
+  const handleOnWheel = (e) => {
+    const { deltaX, deltaY } = e;
 
-    if (!isAtBottom) e.stopPropagation();
+    if (Math.abs(deltaX) < Math.abs(deltaY)) return;
+    else {
+      changeGalleryIndex(deltaX);
+      e.stopPropagation();
+    }
+  };
+
+  const previousTouchPositionsRef = useRef({ x: 0, y: 0 });
+
+  const handleTouchEvents = (e) => {
+    switch (e.type) {
+      case "touchstart":
+        previousTouchPositionsRef.current = {
+          x: e.targetTouches[0].clientX,
+          y: e.targetTouches[0].clientY,
+        };
+        break;
+
+      case "touchmove":
+        const previousTouchPositions = previousTouchPositionsRef.current;
+        const deltaX = previousTouchPositions.x - e.targetTouches[0].clientX;
+        const deltaY = previousTouchPositions.y - e.targetTouches[0].clientY;
+
+        if (Math.abs(deltaX) < Math.abs(deltaY)) return;
+        else {
+          changeGalleryIndex(deltaX);
+          e.stopPropagation();
+        }
+
+        previousTouchPositionsRef.current = {
+          x: e.targetTouches[0].clientX,
+          y: e.targetTouches[0].clientY,
+        };
+
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const changeGalleryIndex = (deltaX) => {
+    if (wheelDebounce.current) {
+      wheelDebounce.current = false;
+
+      if (deltaX > 0 && galleryIndex < maxGalleryIndex) {
+        setGalleryIndex((prev) => prev + 1);
+      } else if (deltaX < 0 && galleryIndex > 0) {
+        setGalleryIndex((prev) => prev - 1);
+      }
+
+      setTimeout(() => {
+        wheelDebounce.current = true;
+      }, 500);
+    }
   };
 
   return (
@@ -121,9 +148,9 @@ const ModuleWrapper = ({ medias, inlineStyle, customColors, children }) => {
         className={styles.moduleWrapper}
         style={inlineStyle}
         ref={sectionRef}
-        onScroll={scrollTrack}
-        onWheel={handleStopPropagation}
-        onTouchMove={handleStopPropagation}
+        onWheel={handleOnWheel}
+        onTouchStart={handleTouchEvents}
+        onTouchMove={handleTouchEvents}
       >
         {children}
       </section>
