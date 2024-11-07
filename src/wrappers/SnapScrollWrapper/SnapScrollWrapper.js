8d58df5e59b9ab7a8b79b8680950815c050b7b72
Damien Bourcheix <damien@mbp-de-damien.home>
2024-11-07 16:22:53 +0100

    EndScrollPanel removed

--- src/wrappers/SnapScrollWrapper/SnapScrollWrapper.js
+++ src/wrappers/SnapScrollWrapper/SnapScrollWrapper.js
@@ -12,19 +12,10 @@ import useScrollTracker from "@/hooks/useScrollTracker";
 const SnapScrollWrapper = ({ children }) => {
   // The content is shown one section at a time. This state stores the coordinates of the active one (the one currently displayed)
 
-  const {
-    updateContainerPos,
-    endScrollValue,
-    setEndScrollValue,
-    showEndScrollPanel,
-  } = useContext(LayoutContext);
+  const { updateContainerPos } = useContext(LayoutContext);
   const containerRef = useRef(null);
 
-  const {
-    scrollTrack,
-    displayIndex: activeChildIndex,
-    scrollPosition,
-  } = useScrollTracker();
+  const { scrollTrack, displayIndex: activeChildIndex } = useScrollTracker();
 
   // At every activeChildIndex change, updates the container position
   useEffect(() => {
@@ -32,80 +23,9 @@ const SnapScrollWrapper = ({ children }) => {
     updateContainerPos(slider, activeChildIndex);
   }, [activeChildIndex, updateContainerPos]);
 
-  const previousTouchYRef = useRef(null);
-
-  // If the user scrolls down while at the bottom of the element, updates the endScrollValue
-  const handleOnWheel = (e) => {
-    const container = containerRef.current;
-
-    const isAtBottom =
-      Math.abs(
-        scrollPosition - (container.scrollHeight - container.offsetHeight)
-      ) <= 1;
-
-    // If the container is not at the bottom, stop the function
-    if (!isAtBottom) return;
-
-    // Accumulates the downward scroll offset
-    const { deltaY } = e;
-    const newEndScrollValue = endScrollValue + deltaY;
-
-    // If the accumulated scroll doesn't exceed the container's height, update the state
-    if (newEndScrollValue <= container.offsetHeight && newEndScrollValue >= 0)
-      setEndScrollValue(newEndScrollValue);
-    else if (endScrollValue >= container.offsetHeight && !showEndScrollPanel)
-      setEndScrollValue(0);
-  };
-
-  // If the user scrolls on touch down while at the bottom of the element, updates the endScrollValue
-  const handleTouchMove = (e) => {
-    const { clientY } = e.changedTouches[0];
-    const previousTouchY = previousTouchYRef.current;
-
-    const container = containerRef.current;
-
-    const isAtBottom =
-      Math.abs(
-        scrollPosition - (container.scrollHeight - container.offsetHeight)
-      ) <= 1;
-
-    // If the container is not at the bottom, stop the function
-    if (!isAtBottom) return;
-
-    // If it's the first touch, skip this part
-    if (previousTouchY) {
-      const deltaY = previousTouchY - clientY;
-
-      // Accumulates the downward scroll offset with a multiplier to facilitate the movement
-      const newEndScrollValue = (endScrollValue + deltaY) * 1.25;
-
-      // If the accumulated scroll doesn't exceed the container's height, update the state
-      if (newEndScrollValue <= container.offsetHeight && newEndScrollValue >= 0)
-        setEndScrollValue(newEndScrollValue);
-      else if (endScrollValue >= container.offsetHeight && !showEndScrollPanel)
-        setEndScrollValue(0);
-
-      // Reset previousTouchYRef if it's last touch
-      setTimeout(() => {
-        if (clientY === previousTouchYRef.current) {
-          previousTouchYRef.current = null;
-        }
-      }, 500);
-    }
-
-    // Update the ref
-    previousTouchYRef.current = clientY;
-  };
-
   return (
     // This wrapper acts as a window to display the content. It should not overflow the client's screen
-    <div
-      className={styles.slider}
-      ref={containerRef}
-      onScroll={scrollTrack}
-      onWheel={handleOnWheel}
-      onTouchMove={handleTouchMove}
-    >
+    <div className={styles.slider} ref={containerRef} onScroll={scrollTrack}>
       {children}
     </div>
   );
