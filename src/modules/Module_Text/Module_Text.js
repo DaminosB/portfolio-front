a4b8607bd1288f238baf9abae7b94730f0f822cc
Damien Bourcheix <damien@mbp-de-damien.home>
2024-11-07 16:16:01 +0100

    Padding on small screens reduced

--- src/modules/Module_Text/Module_Text.js
+++ src/modules/Module_Text/Module_Text.js
@@ -4,16 +4,23 @@ import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 import ModuleWrapper from "@/constructors/ModuleWrapper/ModuleWrapper";
 import TextWrapper from "@/constructors/TextWrapper/TextWrapper";
 import generateInlineStyle from "@/utils/generateInlineStyle";
+import ModuleColumn from "@/constructors/ModuleColumn/ModuleColumn";
 
 const Module_Text = ({ module, customColors }) => {
   const { sectionStyle } = generateInlineStyle(module);
 
   return (
-    <ModuleWrapper inlineStyle={sectionStyle} customColors={customColors}>
+    <ModuleWrapper
+      inlineStyle={sectionStyle}
+      customColors={customColors}
+      module={module}
+    >
       <div className={styles.content}>
-        <TextWrapper textModule={module.text}>
-          <BlocksRenderer content={module.text.richText} />
-        </TextWrapper>
+        <ModuleColumn>
+          <TextWrapper textModule={module.text}>
+            <BlocksRenderer content={module.text.richText} />
+          </TextWrapper>
+        </ModuleColumn>
       </div>
     </ModuleWrapper>
   );
