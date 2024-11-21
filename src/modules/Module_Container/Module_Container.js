671071674768bbd22388a06deb9e58f3dcd2f882
Damien Bourcheix <damien@mbp-de-damien.home>
2024-11-21 18:13:07 +0100

    Ghost divs bugged display corrected

--- src/modules/Module_Container/Module_Container.js
+++ src/modules/Module_Container/Module_Container.js
@@ -1,16 +1,15 @@
 import styles from "./Module_Container.module.css";
 
-import { Fragment } from "react";
-import MediaCardWrapper from "@/constructors/MediaCardWrapper/MediaCardWrapper";
 import ModuleWrapper from "@/constructors/ModuleWrapper/ModuleWrapper";
+import ModuleColumn from "@/constructors/ModuleColumn/ModuleColumn";
 import TextWrapper from "@/constructors/TextWrapper/TextWrapper";
+import MediasGallery from "@/constructors/MediasGallery/MediasGallery";
+import MediaCardWrapper from "@/constructors/MediaCardWrapper/MediaCardWrapper";
 import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 
 import generateCssClasses from "@/utils/generateCssClasses";
 import generateInlineStyle from "@/utils/generateInlineStyle";
-import MediasGallery from "@/constructors/MediasGallery/MediasGallery";
 import populateCardsIdsArray from "@/utils/populateCardsIdsArray";
-import ModuleColumn from "@/constructors/ModuleColumn/ModuleColumn";
 
 const Module_Container = ({ module, customColors }) => {
   const { mediaBlocks, text } = module;
@@ -36,67 +35,60 @@ const Module_Container = ({ module, customColors }) => {
           <div className={styles.mediasContainer} style={mediasContainerStyle}>
             {mediaBlocks.map((mediaBlock, i, array) => {
               if (i % module.imagesPerRow === 0) {
+                const slicedArray = array.slice(i, i + module.imagesPerRow);
+
+                const isSmallerThanChunk =
+                  slicedArray.length < module.imagesPerRow;
+
+                const ghostDivStyle = {
+                  flex: (module.imagesPerRow - slicedArray.length) / 2,
+                };
+
                 return (
                   <div key={mediaBlock.id} style={mediasContainerStyle}>
-                    {array
-                      .slice(i, i + module.imagesPerRow)
-                      .map((subItem, subIndex, subArray) => {
-                        const isSmallerThanChunk =
-                          subArray.length < module.imagesPerRow;
+                    {isSmallerThanChunk && (
+                      <div className={styles.ghost} style={ghostDivStyle}></div>
+                    )}
+                    {slicedArray.map((subItem) => {
+                      return (
+                        <MediasGallery
+                          key={subItem.id}
+                          mediaBlock={subItem}
+                          customColors={customColors}
+                        >
+                          {subItem.mediaAssets.map((mediaAsset, j) => {
+                            const isImageFile =
+                              mediaAsset.provider_metadata.resource_type ===
+                              "image";
 
-                        const ghostDivStyle = {
-                          flex: (module.imagesPerRow - subArray.length) / 2,
-                        };
+                            const mediaCardId =
+                              cardsIdsArray[i * array.length + j];
 
-                        return (
-                          <Fragment key={subItem.id}>
-                            {isSmallerThanChunk && (
-                              <div
-                                className={styles.ghost}
-                                style={ghostDivStyle}
-                              ></div>
-                            )}
-                            <MediasGallery
-                              mediaBlock={subItem}
-                              customColors={customColors}
-                            >
-                              {subItem.mediaAssets.map((mediaAsset, j) => {
-                                const isImageFile =
-                                  mediaAsset.provider_metadata.resource_type ===
-                                  "image";
-
-                                const mediaCardId =
-                                  cardsIdsArray[i * array.length + j];
-
-                                return (
-                                  <MediaCardWrapper
-                                    key={mediaAsset.id}
-                                    customColors={customColors}
-                                    media={mediaAsset}
-                                    cardId={mediaCardId}
-                                  >
-                                    {isImageFile ? (
-                                      <img
-                                        draggable={false}
-                                        src={mediaAsset.url}
-                                        alt={mediaAsset.alternativeText}
-                                      />
-                                    ) : (
-                                      <source src={mediaAsset.url} />
-                                    )}
-                                  </MediaCardWrapper>
-                                );
-                              })}
-                            </MediasGallery>
-                            {isSmallerThanChunk && (
-                              <div
-                                className={styles.ghost}
-                                style={ghostDivStyle}
-                              ></div>
-                            )}
-                          </Fragment>
-                        );
-                      })}
+                            return (
+                              <MediaCardWrapper
+                                key={mediaAsset.id}
+                                customColors={customColors}
+                                media={mediaAsset}
+                                cardId={mediaCardId}
+                              >
+                                {isImageFile ? (
+                                  <img
+                                    draggable={false}
+                                    src={mediaAsset.url}
+                                    alt={mediaAsset.alternativeText}
+                                  />
+                                ) : (
+                                  <source src={mediaAsset.url} />
+                                )}
+                              </MediaCardWrapper>
+                            );
+                          })}
+                        </MediasGallery>
+                      );
+                    })}
+                    {isSmallerThanChunk && (
+                      <div className={styles.ghost} style={ghostDivStyle}></div>
+                    )}
                   </div>
                 );
               }
